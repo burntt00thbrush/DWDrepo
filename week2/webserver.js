40081d47176fdd78c56fe6631df61ee528b04a56
Isa Vento <isavento@github.com>
2018-02-07 19:26:10 -0500

    made changes to week 2

--- week2/webserver.js
+++ week2/webserver.js
@@ -1,21 +1,17 @@
 var express = require('express')
 var app = express()
 
+//config so that 'public' folder will be served
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World!!')
+// })
 
 var count = 0;
 
 var submissions = [];
 
-app.get('/somethingelse', function (req, res) {
-	count++;
-  res.send('<html><body><h1>Something Else' + count + '</h1></body></html>')
-})
-// what is /something else?
 
 app.get('/formpost', function (req, res) {
 	console.log("They submitted: " + req.query.textfield);
@@ -24,29 +20,50 @@ app.get('/formpost', function (req, res) {
     <head>
     <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-    <body><h3>thank you!</h3>
-    <p>you submitted
-  `
-  + req.query.textfield +
-  ` </p>
-    <p><a href='/display'>see all</a></p>
+    <body>
+    <h3>deeply appreciated</h3>
+    <p><a href='/display'>see what others think about</a></p>
     </body>
   </html>
   `;
   res.send(htmlFormPost);
   // save into an array
 submissions.push(req.query.textfield);
-	res.send("You submitted: " + req.query.textfield);
+	res.send("You submitted" );
 	submissions.push(req.query.textfield);
 })
 
 app.get('/display', function(req, res) {
-	var html = "<html><body>";
+  var htmlDisplay = `
+  <html>
+    <head>
+    <link rel= "stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+    <h3> You have provided so many insights</h3> 
+`
+  ;
 	for (var i = 0; i < submissions.length; i++) {
-		html = html + submissions[i] + "<br>";
+    var youtubeUrl = submissions[i]
+
+    //I got help from Barak on the piece of code that includes slice and indexOf
+    //I was having issues with the embedding:
+    //I was getting errors in the console that looked like this
+    //Load denied by X-Frame-Options: does not permit cross-origin framing.
+    //Now the embedding permission is being received by youtube, when I was simply trying
+    //to parse in the youtube URLs being submitted. 
+
+    htmlDisplay+= "<iframe width=" + "\"420\" " + "height=" + "\"315\" " + 
+   "src=\" " + "https://www.youtube.com/embed/" + youtubeUrl.slice(youtubeUrl.indexOf("?v=") + 3) + "\" ></iframe>"; 
+
+
 	}
-	html = html + "</body></html>";
-	res.send(html);
+  htmlDisplay += `
+    <p><a href='/form.html'>go again</a></p>
+    </body>
+    </html>
+  `;
+	res.send(htmlDisplay);
 });
 
 app.listen(3000, function () {
